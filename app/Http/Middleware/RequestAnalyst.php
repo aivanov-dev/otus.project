b9d2835881808826b83a2c28aa0e2208948947a5
snezhig <sneqastk@gmail.com>
2021-07-08 17:23:38 +0300

    Change widget and metrics

--- app/Http/Middleware/RequestAnalyst.php
+++ app/Http/Middleware/RequestAnalyst.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Superbalist\LaravelPrometheusExporter\PrometheusExporter;
 
 class RequestAnalyst
@@ -25,21 +26,40 @@ class RequestAnalyst
     {
         /**@var \Illuminate\Http\Response $response */
         $response = $next($request);
+
+        if (!$this->isMetricPage($request)) {
+            $this->incRequestCount($request, $response);
+            $this->observeResponseTime($request, $response);
+        }
+
+        return $response;
+    }
+
+
+    private function isMetricPage(Request $request): bool
+    {
+        return config('prometheus.metrics_route_path') === $request->path();
+    }
+
+    private function incRequestCount(Request $request, Response $response): void
+    {
         $this->exporter->getOrRegisterCounter(
             'request_count',
             'Count of requests',
-            ['request_type', 'request_code', 'request_route', 'request_uri']
+            ['request_type', 'response_code', 'request_route', 'request_uri']
         )->inc([$request->method(), $response->getStatusCode(), $request->route()?->uri(), $request->getRequestUri()]);
+    }
 
+    private function observeResponseTime(Request $request, Response $response): void
+    {
         $duration = sprintf("%.2f", microtime(true) - LARAVEL_START);
         $this->exporter->getOrRegisterHistogram(
             'response_time_seconds',
             'The response time of a request.',
-            ['request_type', 'request_code', 'request_route', 'request_uri'],
+            ['request_type', 'response_code', 'request_route', 'request_uri'],
         )->observe(
             (float)$duration,
             [$request->method(), $response->getStatusCode(), $request->route()?->uri(), $request->getRequestUri()]
         );
-        return $response;
     }
 }
