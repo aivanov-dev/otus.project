e55324e55019f09f6d304aa0b8c5e72e44476d83
aic513 <aic513@mail.ru>
2021-07-10 18:44:15 +0300

    add user migration & seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory,
+        Notifiable,
+        SoftDeletes;
 
     /**
-     * @var string[]
+     * The attributes that are mass assignable.
+     *
+     * @var array
      */
     protected $fillable = [
-        'name'
+        'name',
     ];
+
+    protected $dates = ['deleted_at'];
 
     /**
      * @var bool
