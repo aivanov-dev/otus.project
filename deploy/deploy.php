43adfd68ea840e05d372c8c5299f327e647e28ca
snezhig <sneqastk@gmail.com>
2021-07-15 21:16:31 +0300

    Commit to try pipeline

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -11,7 +11,7 @@ set('application', 'otus_course');
 set('repository', getenv('BITBUCKET_GIT_HTTP_ORIGIN'));
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+set('git_tty', false);
 
 // Shared files/dirs between deploys
 add('shared_files', []);
