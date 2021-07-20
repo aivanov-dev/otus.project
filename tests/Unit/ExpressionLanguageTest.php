1c988058737b1c296489722e6e12a813aeb5ce49
Alexander * <a3bg5@protonmail.com>
2021-07-20 17:57:07 +0300

    fix bug with new procedure migration and tests, add new expression in seed and tests (first task complete), etc

--- tests/Unit/ExpressionLanguageTest.php
+++ tests/Unit/ExpressionLanguageTest.php
@@ -78,11 +78,19 @@ class ExpressionLanguageTest extends TestCase
         //in app writes expression in such a way they return only true of false
         //все ли задания в занятии выполнены на 10 баллов?
         //так как в контролере будет только $taskResult, то начинаю с $taskResult
-        $expressionAll10 = "taskResult.exercise.taskResults.countWithCondition('assessment', 10) / taskResult.exercise.taskResults.count() == 1";
+        $expressionAll10 = "taskResult.ofTaskAndExerciseAndUser(taskResult.task.exercise.id, taskResult.task_id, taskResult.user_id).countWithCondition('assessment', 10)
+        / taskResult.ofTaskAndExerciseAndUser(taskResult.task.exercise.id, taskResult.task_id, taskResult.user_id).count() == 1";
+
         $this->assertFalse($expressionLanguage->evaluate($expressionAll10, ['taskResult' => $taskResult]));
 
         //доля оценок выше 9 больше 90%?
-        $expression9over90 = "( taskResult.exercise.taskResults.countWithCondition('assessment', 9, '>=') / taskResult.exercise.taskResults.count() ) >= 0.9";
+        $expression9over90 = "taskResult.ofTaskAndExerciseAndUser(taskResult.task.exercise.id, taskResult.task_id, taskResult.user_id).countWithCondition('assessment', 9, '>=')
+         / taskResult.ofTaskAndExerciseAndUser(taskResult.task.exercise.id, taskResult.task_id, taskResult.user_id).count() >= 0.9";
         $this->assertFalse($expressionLanguage->evaluate($expression9over90, ['taskResult' => $taskResult]));
+
+        //первое занятие студента в данном задании и занятии выполнено на любою оценку
+        $expressionFirst = "taskResult.ofTaskAndExerciseAndUser(taskResult.task.exercise.id, taskResult.task_id, taskResult.user_id).isNotEmpty()";
+
+        $this->assertTrue($expressionLanguage->evaluate($expressionFirst, ['taskResult' => $taskResult]));
     }
 }
